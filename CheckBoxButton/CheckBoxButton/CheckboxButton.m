fb78280ad827c379035f30e68f485adeca2a826a
Saeed Hashim <saeedhshm@gmail.com>
2018-01-09 14:59:08 +0200

    after modifications

--- CheckBoxButton/CheckBoxButton/CheckboxButton.m
+++ CheckBoxButton/CheckBoxButton/CheckboxButton.m
@@ -9,19 +9,39 @@
 #import "CheckboxButton.h"
 
 @implementation CheckboxButton
+{
+    UIImage * img;
+}
 
 -(void)awakeFromNib{
     [super awakeFromNib];
     self.isChecked = YES;
-    [self setImage:[UIImage imageNamed:@"unchecked"] forState:UIControlStateNormal];
+//
     [self addTarget:self action:@selector(checkAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.layer setBorderColor:UIColor.lightGrayColor.CGColor];
+    [self.layer setCornerRadius:4];
+    [self.layer setBorderWidth:2];
+    img = [UIImage imageNamed:@"ic_check"];
+    UIImage *image = [img imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    [self setImage:image forState:UIControlStateNormal];
+    self.tintColor = [UIColor whiteColor];
+    [self setBackgroundColor:UIColor.whiteColor];
 }
 
 -(void)checkAction:(UIButton *)sender{
     self.isChecked = !self.isChecked;
-    NSString *imgName = self.isChecked ? @"unchecked":@"checked";
-
-    [self setImage:[UIImage imageNamed:imgName] forState:UIControlStateNormal];
+     [self setBackgroundColor:UIColor.whiteColor];
+    if(self.isChecked){
+      [self setImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+        [self.layer setBorderWidth:2];
+        [self setBackgroundColor:UIColor.whiteColor];
+    }else{
+        [self setBackgroundColor:UIColor.blueColor];
+        [self setImage:img forState:UIControlStateNormal];
+        [self.layer setBorderWidth:0];
+        
+    }
+    
     
 }
 @end
